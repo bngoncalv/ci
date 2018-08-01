3498ff9af0b26d8af7379676fb53562cfa17f12f
bngoncalv <39749951+bngoncalv@users.noreply.github.com>
2018-07-31 23:11:17 -0300

    Update deploy.sh

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -4,7 +4,7 @@
 set -e
 # Lets write the public key of our aws instance
 eval $(ssh-agent -s)
-echo "$PRIVATE_KEY" | tr -d '\r' | ssh-add - > /dev/null
+echo -n "$PRIVATE_KEY" | tr -d '\r' | ssh-add - > /dev/null
 
 # ** Alternative approach
 # echo -e "$PRIVATE_KEY" > /root/.ssh/id_rsa
